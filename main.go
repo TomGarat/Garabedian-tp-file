187fecd862ff376171a39a06ff849f9c3c07479b
Tom Garabedian <tomgarat@MacBook-Pro-de-Tom.local>
2022-10-05 16:51:38 +0200

    random

--- main.go
+++ main.go
@@ -2,25 +2,33 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func main() {
-	file, _ := os.ReadFile("File.txt")
-	lignes := strings.Split(string(file), "\r")
-	fmt.Print(lignes[0],
-		lignes[len(lignes)-1])
-	for i, e := range lignes {
-		if strings.Contains(e, "before") {
-			e_pos, _ := strconv.Atoi(lignes[i+1][1:3])
-			fmt.Print(lignes[e_pos-1])
-		} else if strings.Contains(e, "now") {
-			mot_ascii := []byte(lignes[i-1])
-
-			e_pos := int(mot_ascii[2]) / len(lignes)
-			fmt.Println(lignes[(e_pos)-1])
+	file, _ := os.ReadFile("File.txt")          // on lit le fichier File.txt
+	lignes := strings.Split(string(file), "\r") // on sépare le fichier en lignes
+	fmt.Print(lignes[0],                        // on affiche la première ligne
+		lignes[len(lignes)-1]) // on affiche la dernière ligne
+	for i, e := range lignes { // on parcourt les lignes
+		if strings.Contains(e, "before") { // si la ligne contient "before" alors :
+			e_pos, _ := strconv.Atoi(lignes[i+1][1:3]) // on récupère la position de la ligne suivante
+			fmt.Print(lignes[e_pos-1])                 // on affiche la ligne correspondante
+		} else if strings.Contains(e, "now") { // si la ligne contient "now" alors :
+			mot_ascii := []byte(lignes[i-1])         // on récupère la ligne précédente
+			e_pos := int(mot_ascii[2]) / len(lignes) // on récupère la position de la lettre à afficher
+			fmt.Println(lignes[(e_pos)-1])           // on affiche la ligne correspondante
 		}
 	}
+	randnumber() // on affiche un nombre aléatoire
+}
+
+func randnumber() { // fonction qui affiche un nombre aléatoire
+	rand.Seed(time.Now().UnixNano()) // on initialise le générateur de nombres aléatoires
+	i := rand.Intn(0-50) + 0         // on génère un nombre aléatoire entre 0 et 50
+	fmt.Println(i)                   // on affiche le nombre aléatoire
 }
